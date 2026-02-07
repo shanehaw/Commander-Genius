d8552ab916255ad2bf75ed41a1e4eb173fa51c34
Shane Haw <10742171+shanehaw@users.noreply.github.com>
2026-02-07 09:54:35 +0000

    Keep trying to fix the pre-processor conditionals

--- GsKit/base/Configurator.h
+++ GsKit/base/Configurator.h
@@ -12,6 +12,22 @@
 #ifndef CONFIGURATOR_H_
 #define CONFIGURATOR_H_
 
+// Include Apple headers if on Apple platform
+#if defined(__APPLE__)
+    #include <TargetConditionals.h>
+#endif
+
+// Define fallbacks for non-Apple platforms
+#ifndef TARGET_OS_IPHONE
+    #define TARGET_OS_IPHONE 0
+#endif
+#ifndef TARGET_IPHONE_SIMULATOR
+    #define TARGET_IPHONE_SIMULATOR 0
+#endif
+#ifndef TARGET_OS_IOS
+    #define TARGET_OS_IOS 0
+#endif
+
 
 #if defined(ANDROID) || TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
     #define EMBEDDED
